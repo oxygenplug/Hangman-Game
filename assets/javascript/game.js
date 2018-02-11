7d3db39a8f71986153847db3c61bc01af4151eb7
oxygenplug <dan.garces2@gmail.com>
2018-02-10 19:02:42 -0500

    changed to writing the game as an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,37 @@
-var makeSpaces = function (spaces) {
-    var blank = " ";
-    for (var i = 0; i < spaces; i++) {
-        blank += "_ ";
-
-    }
-    return blank.slice(0, -1);
-}/* 
+/* set up
+ - create array holding our words to use in game
+ - create array to hold letters guessed
+ - create object with wins/losses
 
 
 */
+var hangman = {
+
+    poolOfWords: ["map", "pirate", "treasure", "parrot", "ship", "captain", "crew", "mutiny", "sail"],
+    chosenWord: "",
+    chooseWord: function () {
+        chosenWord = poolOfWords[Math.floor(Math.random() * poolOfWords.length)];
+        console.log(chosenWord)
+    },
+
+    makeSpaces: function () {
+        var blank = " ";
+        for (var i = 0; i < chosenWord.length; i++) {
+            blank += "_ ";
+
+        }
+        console.log(blank.slice(0, -1));
+    },
+    winLossCounter: {
+        wins: 0,
+        losses: 0,
+    },
+
+
+    checkGuess: function () {
+        if (event.key === chosenWord.includes()) {
+
+        }
+    },
+
+}
