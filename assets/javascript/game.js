bd60e42a54a47fed0de96c9b60300724538ea8b1
oxygenplug <dan.garces2@gmail.com>
2018-02-10 22:14:17 -0500

    changed order of properties

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,19 @@
 
 */
 var hangman = {
+
     chosenWord: "",
     poolOfWords: ["map", "pirate", "treasure", "parrot", "ship", "captain", "crew", "mutiny", "sail"],
+    gameRunning: false,
+    wins: 0,
+    losses: 0,
+    partialAnswer: "",
+
     chooseWord: function () {
         this.chosenWord = this.poolOfWords[Math.floor(Math.random() * this.poolOfWords.length)];
     },
 
 
-        wins: 0,
-        losses: 0,
 
 
     checkGuess: function () {
@@ -23,33 +27,35 @@ var hangman = {
         }
     },
 
-    partialAnswer: "",
 
-    createPartialAnswer: function() {
+
+    createPartialAnswer: function () {
         for (var i = 0; i < this.chosenWord.length; i++) {
             this.partialAnswer += "_";
- 
-         }
+
+        }
     },
 
-    guessLetter: function(guessedLetter){
-        for(var i = 0; i < answer.length; i++){
-            if(guessedLetter == answer.charAt(i)){
-                 hiddenWord[i] = guessedLetter;
+    guessLetter: function (guessedLetter) {
+        for (var i = 0; i < answer.length; i++) {
+            if (guessedLetter == answer.charAt(i)) {
+                hiddenWord[i] = guessedLetter;
             }
         }
-        },
+    },
 
-        startGame: function() {
-            this.chooseWord();
-            this.createPartialAnswer();
-            console.log(this.chosenWord, this.PartialAnswer);
-    
-        },
+    startGame: function () {
+        this.chooseWord();
+        this.createPartialAnswer();
+        console.log(this.chosenWord, this.PartialAnswer);
+
+    },
+
+
 
 }
 
 console.log("it ran");
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     hangman.startGame();
 }
