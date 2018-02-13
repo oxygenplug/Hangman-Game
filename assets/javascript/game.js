4b0cfe155b74f9201b565d0c190a032d4339af88
oxygenplug <dan.garces2@gmail.com>
2018-02-12 22:31:31 -0500

    mostly working. figuring out how to obtain the input from the text area

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,7 +35,7 @@ var hangman = {
         if (this.isCorrectGuess(letter)) {
             return;
         }
-        
+
         this.madeBadGuess(letter);
     },
 
@@ -67,15 +67,15 @@ var hangman = {
             }
             value += " ";
         }
-        this.wordBeingGuessedElement.innerText=value;
+        this.wordBeingGuessedElement.innerText = value;
     },
 
     printBadGuess: function () {
-        for (i = 0; i < this.badGuesses.length; i++) {
-            this.badGuessesElement.innerText=this.badGuesses;
-        }
 
-        this.showLivesLeftElement.innerText=this.livesRemaining;
+        this.badGuessesElement.innerText = this.badGuesses;
+
+
+        this.showLivesLeftElement.innerText = this.livesRemaining;
 
     },
 
@@ -83,10 +83,9 @@ var hangman = {
 
     startGame: function () {
         this.gameRunning = true,
-        this.chooseWord();
+            this.chooseWord();
         this.createCorrectCharacters();
         this.print();
-        this.endGame();
     },
 
 
@@ -95,25 +94,25 @@ var hangman = {
         this.badGuesses.push(letter);
 
         this.livesRemaining--;
-           
 
-        if (this.livesRemaining == 0) {
-            console.log("GAME OVER");
 
-        }
+        
         this.printBadGuess();
         console.log(this.livesRemaining);
 
+        if (this.livesRemaining == 0) {
+            this.endGame();
 
+        }
 
     },
 
-    endGame: function() {
-        if(this.livesRemaining == 0) {
+    endGame: function () {
+        
             alert('Click "Game Over" to try again!');
-            this.gameRunning=false;
+            this.gameRunning = false;
             document.getElementById('toggleGameOver').classList.add('displayGameOver');
-        }
+        
     },
 
 
@@ -130,122 +129,7 @@ document.onkeyup = function (event) {
     hangman.print();
 };
 
-/* var createHangman = function () {
-    return {
-    // initializing all properties 
-    chosenWord: "",
-    poolOfWords: ["map", "pirate", "treasure", "parrot", "ship", "captain", "crew", "mutiny", "sail"],
-    gameRunning: false,
-    wins: 0,
-    losses: 0,
-    correctCharacters: [],
-    badGuesses: [],
-    livesRemaining: 10,
-    wordBeingGuessedElement: document.getElementById('wordBeingGuessed'),
-    badGuessesElement: document.getElementById('badGuessList'),
-    showLivesLeftElement: document.getElementById('showLivesLeft'),
-    //initializing all my methods
-    // randomly chooses a word from my array of words
-    chooseWord: function () {
-        this.chosenWord = this.poolOfWords[Math.floor(Math.random() * this.poolOfWords.length)];
-    },
 
-
-
-
-    createCorrectCharacters: function () {
-        for (var i = 0; i < this.chosenWord.length; i++) {
-            this.correctCharacters.push(false);
-        }
-    },
-
-    // logic for guessing letters.
-    guessLetter: function (letter) {
-        if (this.isAlreadyGuessed(letter)) {
-            return;
-        }
-        if (this.isCorrectGuess(letter)) {
-            return;
-        }
-        
-        this.madeBadGuess(letter);
-    },
-
-    isAlreadyGuessed: function (letter) {
-        return this.badGuesses.find((item) => {
-            return letter == item;
-        });
-    },
-
-    isCorrectGuess: function (letter) {
-        var isCorrectGuess = false;
-        for (var index = 0; index < this.chosenWord.length; index++) {
-            if (letter == this.chosenWord.charAt(index)) {
-                isCorrectGuess = true;
-                this.correctCharacters[index] = true;
-            }
-        }
-        return isCorrectGuess;
-    },
-
-    print: function () {
-        var value = "";
-        for (var i = 0; i < this.correctCharacters.length; i++) {
-            if (!this.correctCharacters[i]) {
-                value += "_";
-            }
-            else {
-                value += this.chosenWord[i];
-            }
-            value += " ";
-        }
-        this.wordBeingGuessedElement.innerText=value;
-    },
-
-    printBadGuess: function () {
-        for (i = 0; i < this.badGuesses.length; i++) {
-            this.badGuessesElement.innerText=this.badGuesses;
-        }
-
-        this.showLivesLeftElement.innerText=this.livesRemaining;
-
-    },
-
-    // starts the game when called
-
-    startGame: function () {
-        this.gameRunning = true,
-        this.chooseWord();
-        this.createCorrectCharacters();
-        this.print();;
-    },
-
-
-    // lowers lives remaining every time an incorrect guess is made (this is checked by looking at the  value of the badGuesses array
-    madeBadGuess: function (letter) {
-        this.badGuesses.push(letter);
-
-        this.livesRemaining--;
-           
-
-        if (this.livesRemaining == 0) {
-            console.log("GAME OVER");
-
-        }
-        this.printBadGuess();
-        console.log(this.livesRemaining);
-
-
-
-    },
-
-    endGame: function() {
-        if(this.livesRemaining == 0) {
-            alert('Click "Game Over" to try again!');
-            this.gameRunning=false;
-            
-        }
-    },
-
-
-}; */
+var reloadPage = function () {
+    location.reload();
+};
